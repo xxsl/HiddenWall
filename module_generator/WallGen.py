108db2747ab0aacf60f18e620ee9763686e26d6e
CoolerVoid <coolerlair@gmail.com>
2019-04-21 13:36:24 -0300

    update

--- module_generator/WallGen.py
+++ module_generator/WallGen.py
@@ -1,53 +1,41 @@
 #!/usr/bin/python3
-
+# WallGen v0.1
 #You nd yaml, pyyaml modules... 
 from util import parser
+from util import file_op
 
 template_filename=""
 rules_filename=""
 template_content=""
 template_content2=""
-souce=""
+source=""
 
+# Get argvs of user's input
 template_filename,rules_filename = parser.arguments()
 
 # load rules of firewall at directory rules
 v=parser.Get_config(rules_filename)
 
-# Load templates
-with open(template_filename) as f:
- content = f.readlines()
-
-for line in content:
- template_content+=line
-
-with open("template/Makefile") as f:
- content = f.readlines()
-
-for line in content:
- template_content2+=line
+# Load content of templates
+template_content=file_op.File_to_string(template_filename)
+template_content2=file_op.File_to_string("template/Makefile")
 
 print ("\n Generate Kernel module \n")
 
-# replace macros in templates
+# replace macros in C module template
 source=template_content
 source=source.replace("PUBLIC_PORTS",v['public_ports'])
 source=source.replace("IP_WHITELISTED",v['whitelist_code'])
 source=source.replace("PORTS_COUNT",v['open_port_count'])
 
+# replace macro in Makefile
 source2=template_content2
 source2=source2.replace("FILE_NAME",v['module_name'])
 
 # save output
 name_file="output/"+v['module_name']+".c"
-
-file = open(name_file,"w") 
-file.write(source) 
-file.close()
-
-file = open("output/Makefile","w") 
-file.write(source2) 
-file.close()
+file_op.WriteFile(name_file,source)
+file_op.WriteFile("output/Makefile",source2)
 
 print ("Python Script end, please look the output your custom kernel module at directory output/\n")
 
